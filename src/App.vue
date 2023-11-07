e2c09771eab35b01c1287be609a9adbe96c8d6ae
Luz Rocío García Peña <luz.rocio.garci@gmail.com>
2023-11-07 10:32:48 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <span>Prueba jdkjskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkjsdnnnnnnnnnnnncksiijqwhdwhdhuahkuejbdkjbkjhcjabk</span>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+body {
+  background-color: #82C3C7;
+}
+</style>
---
+++ src/components/encabezado.vue
@@ -0,0 +1,14 @@
+<template>
+    <span>Chismecito.com</span>
+    
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+body {
+    background-color: black;
+}
+</style>
