a4d6793ddddf920d13dabd21d0c09205c10e59c9
Luz Rocío García Peña <luz.rocio.garci@gmail.com>
2023-11-07 20:57:00 -0600

    Inicio de tarjeta de chismes

--- src/components/tarjeta_chisme.vue
+++ src/components/tarjeta_chisme.vue
@@ -1,5 +1,19 @@
 <template>
-
+    <v-container>
+        <v-layout>
+            <v-flex>
+                <v-card>
+                    <v-card-title>
+                        Datos del encabezado
+                    </v-card-title>
+                    <v-card-body>
+                        Contenido de la carta
+                    </v-card-body>
+                    Aquí deben ir los botones
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script setup>
