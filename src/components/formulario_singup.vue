f4764a2e331be1903cf0a220980967137cafa416
Ibis Carrillo Araujo <ibisca03@gmail.com>
2023-11-08 13:55:24 -0600

    Inicio creacion comp singup

---
+++ src/components/formulario_singup.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="container">
+        <span> CREA TU CUENTA </span>
+        <input_personalizable id="fechaNacimiento" type="date"></input_personalizable>
+    </div>
+</template>
+
+<script setup>
+import input_personalizable from './input_personalizable.vue';
+</script>
+
+<style setup></style>
