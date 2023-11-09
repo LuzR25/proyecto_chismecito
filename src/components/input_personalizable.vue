768fbf8900fefab745630a1b88539a44f9bb815a
Ibis Carrillo Araujo <ibisca03@gmail.com>
2023-11-09 08:25:01 -0600

    mostrar placeholder en tipo date

--- src/components/input_personalizable.vue
+++ src/components/input_personalizable.vue
@@ -4,8 +4,12 @@
         + :value="variablePadre" es el parametro que se pasa a traves del padre. En este se caso el padre va a pasar una variable en la cual se guardara el dato que el usuario escribe en el campo de entrada.
         + @input esta accion se realiza cuando el usuario escribe en el campo de entrada y cuando sucede esto se ejecuta la función "updateValue".
     -->
-    <input v-if="type !== 'select'" :id="id" :type="type" :placeholder="hint" :value="variablePadre" @input="updateValue" />
-    <!-- Cuando el input sea de tipo 'select' -->
+    <input v-if="type !== 'select' && type !== 'date'" :id="id" :type="type" :placeholder="hint" :value="variablePadre"
+      @input="updateValue" />
+    <!-- input de tipo date (fecha) -->
+    <input v-else-if="type === 'date'" :id="id" :type="fechaEnTipo" :placeholder="hint" :value="variablePadre"
+      @focus="cambiarFechaADate" @input="updateValue" />
+    <!-- input de tipo select (seleccion) -->
     <select v-else :id="id" :value="variablePadre" @change="updateValue">
       <option value="" disabled selected>{{ hint }}</option>
       <option value="masculino">Masculino</option>
@@ -16,7 +20,7 @@
 </template>
 
 <script setup>
-import { toRefs, defineEmits } from 'vue';
+import { ref, toRefs, defineEmits } from 'vue';
 
 // Parametros
 const props = defineProps(['id', 'type', 'hint', 'variablePadre'])
@@ -35,24 +39,33 @@ const updateValue = (event) => {
   */
   emit('update:variablePadre', event.target.value)
 }
+
+// constantes para hacer que el input de fecha muestre un placeholder
+let fechaEnTipo = ref('text')
+
+/* Al inicializarse el input de la fecha esta en tipo techo, pero una vez que se da clic sobre el input de fecha esta funcion la cambiara a tipo date */
+const cambiarFechaADate = () => {
+  fechaEnTipo.value = 'date';
+}
+
 </script>
 
 <style scoped>
-
 div {
   margin-top: 5%;
 }
-input, select {
-  font-family: 'Abel', sans-serif;
+
+input,
+select {
   font-size: 20px;
   background-color: white;
   border: 1.5px solid black;
   border-radius: 15px;
   padding-left: 5%;
+  padding-right: 5%;
   height: 8vh;
   width: 50vw;
 }
-
 </style>
 
 <!-- <template>
