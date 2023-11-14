0abff12f36980f52dbaa4fd00134c01524fc8f8e
Ibis Carrillo Araujo <ibisca03@gmail.com>
2023-11-13 18:18:23 -0600

    Eliminar vutify

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
+//Diseños de css globales para componentes que siempre se verán igual
+import './assets/global.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-// import '@mdi/font/css/materialdesignicons.css'
-
-// VueRouter
-// configuración
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-  }
-})
-
-createApp(App).use(vuetify).mount('#app')
+createApp(App).mount('#app')
