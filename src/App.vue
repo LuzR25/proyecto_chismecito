a011a1d0fde1ea3b8252879a83eef0dc6ad3ef98
Ibis Carrillo Araujo <ibisca03@gmail.com>
2023-11-13 18:26:07 -0600

    probando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,13 @@
 <template>
-  <!-- <formulario_login></formulario_login> -->
-  <formulario_singup></formulario_singup>
+  <!-- <formulario_login></formulario_login>
+  <formulario_singup></formulario_singup> -->
+  <informacion_cuenta></informacion_cuenta>
 </template>
 
 <script setup>
 import formulario_login from './components/formulario_login.vue';
 import formulario_singup from './components/formulario_singup.vue';
-
+import informacion_cuenta from './components/informacion_cuenta.vue';
 </script>
 
-<style>
-body {
-  background-color: #82C3C7;
-}
-
-.formularios {
-  font-family: 'Abel', sans-serif;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.spanFormulario {
-  font-size: 40px;
-}
-</style>
-
-<!-- <template>
-  <boton_personalizable texto="INGRESAR" color="#EBCE7D" :accion="miFuncion" />
-</template>
-
-<script setup>
-import boton_personalizable from './components/boton_personalizable.vue';
-
-const miFuncion = () => {
-  console.log('¡Botón presionado!')
-}
-</script> -->
-
-
-<!-- <template>
-  <span>INICIAR SESIÓN</span>
-  <div id="app">
-    <input_personalizable id="email" type="email" hint="Correo electrónico" :variablePadre="email" @update:variablePadre="email = $event" />
-    <input_personalizable id="password" type="password" hint="Contraseña" :variablePadre="password" @update:variablePadre="password = $event" />
-    <boton_personalizable texto="INGRESAR" color="#EBCE7D" :accion="submit" />
-  </div>
-</template>
-
-<script setup>
-import input_personalizable from './components/input_personalizable.vue';
-import boton_personalizable from './components/boton_personalizable.vue';
-
-let email = '';
-let password = '';
-
-const submit = () => {
-  console.log(`Correo electrónico: ${email}, Contraseña: ${password}`);
-};
-
-/* const miFuncion = () => {
-  console.log('¡Botón presionado!');
-};
- */
-</script>
-
-<style>
-body {
-  background-color: #82C3C7;
-}
-</style> -->
-
-
+<style scoped></style>
