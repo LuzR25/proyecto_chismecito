0bac9cef11267ff851b4ffebf6aec6022f328816
Ibis Carrillo Araujo <ibisca03@gmail.com>
2023-12-03 01:25:47 -0600

    enviar datos al componente padre

--- src/components/informacion_cuenta.vue
+++ src/components/informacion_cuenta.vue
@@ -4,44 +4,34 @@
         <div class="contenedorInf">
             <label class="labelPrincipal" for="">Fecha de nacimiento</label>
             <!-- class="inputDatos" para agregar el diseño global para los inputs de entrada del sitio web -->
-            <input class="inputDatos" type="date" v-model="props.fechaNacimiento">
+            <input class="inputDatos" type="date" v-model="localFechaNacimiento">
         </div>
         <!-- INPUT GENERO -->
         <div class="contenedorInf">
             <label class="labelPrincipal" for="select">Género</label>
             <!-- Se mostrará el select -->
-            <select v-if="genero == 'masculino'" id="select" class="inputDatos">
-                <option value="masculino" selected>Masculino</option>
-                <option value="femenino">Femenino</option>
-                <option value="noBinario">No Binario</option>
-            </select>
-            <select v-else-if="genero == 'femenino'" id="select" class="inputDatos">
-                <option value="masculino">Masculino</option>
-                <option value="femenino" selected>Femenino</option>
-                <option value="noBinario">No Binario</option>
-            </select>
-            <select v-else="genero == 'noBinario'" id="select" class="inputDatos">
+            <select id="select" class="inputDatos" v-model="localGenero">
                 <option value="masculino">Masculino</option>
                 <option value="femenino">Femenino</option>
-                <option value="noBinario" selected>No Binario</option>
+                <option value="noBinario">No Binario</option>
             </select>
         </div>
         <!-- INPUT USUARIO -->
         <div class="contenedorInf">
             <label class="labelPrincipal" for="">Usuario</label>
-            <input class="inputDatos" type="text" v-model="props.usuario">
+            <input class="inputDatos" type="text" v-model="localUsuario">
         </div>
         <!-- INPUT PASSWORD -->
         <div id="ultimoContenedorInf" class="contenedorInf">
             <label class="labelPrincipal" for="">Contraseña</label>
-            <input class="inputDatos" :type="mostrarPassword ? 'text' : 'password'" v-model="props.password">
+            <input class="inputDatos" :type="mostrarPassword ? 'text' : 'password'" v-model="localPassword">
             <div id="divMostrarPassword">
                 <input type="checkbox" v-model="mostrarPassword">
                 <label for="">Ver contraseña</label>
             </div>
         </div>
         <div id="botones">
-            <boton_personalizable id="modificar" texto="MODIFICAR DATOS DE CUENTA" color="#EBCE7D" accion="">
+            <boton_personalizable id="modificar" texto="MODIFICAR DATOS DE CUENTA" color="#EBCE7D" :accion="enviarDatos">
             </boton_personalizable>
             <boton_personalizable id="eliminar" texto="ELIMINAR CUENTA" color="#C61800"></boton_personalizable>
         </div>
@@ -52,26 +42,44 @@
 import boton_personalizable from './boton_personalizable.vue';
 import { ref } from 'vue';
 
- let mostrarPassword = ref(false);
+let mostrarPassword = ref(false);
 
- const props = defineProps({
-  fechaNacimiento: {
-    type: String,
-    required: true
-  },
-  genero: {
-    type: String,
-    required: true
-  },
-  usuario: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-})
+const props = defineProps({
+    fechaNacimiento: {
+        type: String,
+        required: true
+    },
+    genero: {
+        type: String,
+        required: true
+    },
+    usuario: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+// Create local data properties
+let localFechaNacimiento = ref(props.fechaNacimiento);
+let localGenero = ref(props.genero);
+let localUsuario = ref(props.usuario);
+let localPassword = ref(props.password);
+
+const emit = defineEmits(['actualizar-datos']);
+
+const enviarDatos = () => {
+  emit('actualizar-datos', {
+    nuevaFechaNacimiento: localFechaNacimiento.value,
+    nuevoGenero: localGenero.value,
+    nuevoUsuario: localUsuario.value,
+    nuevoPassword: localPassword.value,
+  });
+};
+
 </script>
 
 <style scoped>
