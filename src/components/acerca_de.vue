9699af1dc4f46270b63dc13d1b2a6cfa39dede34
Acuablast <paramipersona@outlook.es>
2023-12-03 20:35:13 -0600

    Creación componente Acerca de

--- src/components/acerca_de.vue
+++ src/components/acerca_de.vue
@@ -1,10 +1,32 @@
 <template>
-<h1>Acerca de</h1>
-<p>Esto será el acerca de. Por ahora se verá culero.</p>
+<h1 id="title">Acerca de</h1>
+<div id="about">
+    <p id="text">Chismesito.com es una plataforma en línea que permite a los usuarios compartir información de manera anónima. Fue creada por cuatro estudiantes universitarios que se inspiraron en la idea de los chismógrafos de la secundaria. Los usuarios pueden publicar cualquier cosa que deseen, desde chismes hasta noticias, siempre y cuando no infrinjan las políticas de la plataforma. La plataforma también permite a los usuarios comentar y votar en publicaciones de otros usuarios. En resumen, Chismesito.com es una plataforma en línea que permite a los usuarios compartir información de manera anónima y sin restricciones.
+    </p>
+
+</div>
+<div id="footer"></div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+#title{
+    padding: 3vh 0 3vh 0;
+    text-align: center;
+}
+#about{
+    background: white;
+    border-radius: 22px;
+    text-align: justify;
+    width: 90%;
+    margin: auto;
+}
+#text{
+    padding: 20px;
+}
+#footer{
+    height: 55vh;
+}
 </style>
